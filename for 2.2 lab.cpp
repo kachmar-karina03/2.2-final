f63dff303a493e4fd994d8c6a058dc862eecfa8a
kachmar-karina03 <72577529+kachmar-karina03@users.noreply.github.com>
2020-10-15 14:35:40 +0300

    master2

--- for 2.2 lab.cpp
+++ for 2.2 lab.cpp
@@ -17,16 +17,16 @@ int main() {
     double Pi = 4 * atan(1.); // число пі
     double a;    // вхідний параметр
     double z1; // результат обчислення 1-го виразу
-    //double z2; // результат обчислення 2-го виразу
+    double z2; // результат обчислення 2-го виразу
     
     cout << "a="; cin >> a;
   
     z1 = (sin(4 * a) / (1 + cos(4 * a)) * (cos(2 * a)) / (1 + cos(2 * a)));
-    //z2 = cos((3.0 / 2 * Pi) - a) / sin((3. / 2 * Pi) - a);
+    z2 = cos((3.0 / 2 * Pi) - a) / sin((3. / 2 * Pi) - a);
 
     cout << endl;
     cout << "z1 =" << z1 << endl;
-    //cout << "z2 =" << z2 << endl;
+    cout << "z2 =" << z2 << endl;
 
     cin.get();
     system("pause");
